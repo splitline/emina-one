dd89d5e2a1a4c8bbe828c055fbd2526e1965abc0
黃志仁 <tbsthitw@gmail.com>
2020-02-26 04:17:21 +0800

    Improve UI of watch history.

--- screens/WatchHistory.js
+++ screens/WatchHistory.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Share, Linking, Alert } from 'react-native';
 import { connect } from 'react-redux';
-import { Appbar, List, Surface, Card, Title, Paragraph, Avatar } from 'react-native-paper';
+import { Appbar, List, Surface, Card, Title, Paragraph, IconButton } from 'react-native-paper';
 
 const AboutScreen = ({ navigation, history }) => {
+    const formatDate = (date) => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
     return (
         <View>
             <Appbar.Header>
@@ -13,20 +14,21 @@ const AboutScreen = ({ navigation, history }) => {
             {Object.entries(history)
                 .sort((a, b) => b[1].lastWatch - a[1].lastWatch)
                 .map(([animeId, data], i) =>
-                    <List.Accordion
-                        key={i}
-                        title={data.watchedList[0].title}
-                        description={new Date(data.watchedList[0].lastWatch) + ""}
-                        left={props => <List.Icon {...props} icon="folder" />}
-                    >
-                        {data.watchedList.map((item, i) =>
-                            <List.Item
-                                key={i}
-                                title={item.title}
-                                onPress={() => navigation.navigate("Video", { animeId, videoId: item.id })}
-                            />
-                        )}
-                    </List.Accordion>
+                    <Surface key={i} style={{ margin: 8, elevation: 3 }}>
+                        <List.Accordion
+                            title={data.watchedList[0].title}
+                            description={formatDate(new Date(data.watchedList[0].lastWatch))}
+                        >
+                            {data.watchedList.map((item, i) =>
+                                <List.Item
+                                    key={i}
+                                    title={item.title}
+                                    right={() => <List.Subheader>{formatDate(new Date(item.lastWatch))}</List.Subheader>}
+                                    onPress={() => navigation.navigate("Video", { animeId, videoId: item.id })}
+                                />
+                            )}
+                        </List.Accordion>
+                    </Surface>
                 )}
         </View>
     )
