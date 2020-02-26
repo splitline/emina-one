8d2d1c024747142f36ba16dff49a04461b58af28
黃志仁 <tbsthitw@gmail.com>
2020-02-27 00:54:28 +0800

    Fix watchHistory warning.

--- screens/WatchHistory.js
+++ screens/WatchHistory.js
@@ -13,50 +13,49 @@ const WatchHistory = ({ navigation, history, clearHistory }) => {
                 <Appbar.Content title="觀看紀錄" />
                 <Appbar.Action icon="information-outline" onPress={() => navigation.navigate("About")} />
             </Appbar.Header>
-            <ScrollView contentContainerStyle={{ flex: 1 }}>
-                <Button
-                    icon="notification-clear-all"
-                    onPress={() => Alert.alert(
-                        "清除紀錄",
-                        "確定要清除嗎？", [
-                        { text: "確定", onPress: () => clearHistory() },
-                        { text: "取消", style: "cancel" }
-                    ])}
-                    style={{ alignSelf: "flex-end", marginTop: 4, marginRight: 8 }}
-                    compact
-                >
-                    清除紀錄
-                </Button>
-                {Object.keys(history).length === 0 ?
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text>到時候觀看紀錄會出現在這邊</Text>
-                    </View> :
-                    <FlatList
-                        data={Object.entries(history).sort((a, b) => b[1].lastWatch - a[1].lastWatch)}
-                        ItemSeparatorComponent={()=><Divider />}
-                        keyExtractor={(_, index) => index}
-                        renderItem={({ item: [animeId, data] }) =>
-                            <Surface>
-                                <List.Accordion
-                                    title={data.watchedList[0].title}
-                                    titleNumberOfLines={0}
-                                    description={formatDate(new Date(data.watchedList[0].lastWatch))}
-                                >
-                                    {data.watchedList.map((item, i) =>
-                                        <List.Item
-                                            key={i}
-                                            title={item.title}
-                                            titleNumberOfLines={0}
-                                            right={() => <List.Subheader>{formatDate(new Date(item.lastWatch))}</List.Subheader>}
-                                            onPress={() => navigation.navigate("Video", { animeId, videoId: item.id })}
-                                        />
-                                    )}
-                                </List.Accordion>
-                            </Surface>
-                        }
-                    />
-                }
-            </ScrollView>
+            {Object.keys(history).length === 0 ?
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>到時候觀看紀錄會出現在這邊</Text>
+                </View> :
+                <FlatList
+                    ListHeaderComponent={() =>
+                        <Button
+                            icon="notification-clear-all"
+                            onPress={() => Alert.alert(
+                                "清除紀錄",
+                                "確定要清除嗎？", [
+                                { text: "確定", onPress: () => clearHistory() },
+                                { text: "取消", style: "cancel" }
+                            ])}
+                            style={{ alignSelf: "flex-end", margin:4 }}
+                            compact
+                        >
+                            清除紀錄
+                        </Button>}
+                    data={Object.entries(history).sort((a, b) => b[1].lastWatch - a[1].lastWatch)}
+                    ItemSeparatorComponent={() => <Divider />}
+                    keyExtractor={(item) => item[0]}
+                    renderItem={({ item: [animeId, data] }) =>
+                        <Surface>
+                            <List.Accordion
+                                title={data.watchedList[0].title}
+                                titleNumberOfLines={0}
+                                description={formatDate(new Date(data.watchedList[0].lastWatch))}
+                            >
+                                {data.watchedList.map((item, i) =>
+                                    <List.Item
+                                        key={i}
+                                        title={item.title}
+                                        titleNumberOfLines={0}
+                                        right={() => <List.Subheader>{formatDate(new Date(item.lastWatch))}</List.Subheader>}
+                                        onPress={() => navigation.navigate("Video", { animeId, videoId: item.id })}
+                                    />
+                                )}
+                            </List.Accordion>
+                        </Surface>
+                    }
+                />
+            }
         </View>
     )
 }
